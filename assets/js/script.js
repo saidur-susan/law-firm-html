f84dd2f83d71b54dd992b679a59878330f260086
saidur-hasan <saidur.susan@gmail.com>
2026-02-07 19:26:32 +0600

    carosel fixed on testimonila

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,91 @@ menuBtn.addEventListener("click", () => {
     mobileMenu.classList.toggle("hidden");
     menuBtn.classList.toggle("open");
 });
+
+
+jQuery(document).ready(function () {
+    $('.attorney-wrapper').slick({
+
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+
+    });
+
+    $('.blog-wrapper').slick({
+
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+
+
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+
+    });
+
+    $(".testimonial-wrapper").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: true,
+        dots: false,
+        arrows: false
+    });
+});
